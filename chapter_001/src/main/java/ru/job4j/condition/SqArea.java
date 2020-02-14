9471435a87cb2060d94cad45e4b31e7dd26f8879
Into Matar <intomatar@gmail.com>
2020-02-15 04:41:49 +0600

    Little fix

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,15 +1,10 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        double h = p / (2 * (k + 1));
-        double L = h * k;
-        double s = L * h;
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
         return s;
-    }
-
-    public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -6,17 +6,17 @@ import static org.junit.Assert.assertThat;
 
 public class MultiMaxTest {
     @Test
-    public void whenSecondMax() {
-        MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
-        assertThat(result, is(4));
-    }
-
-    @Test
     public void whenFirstMax() {
         MultiMax check = new MultiMax();
         int result = check.max(5, 4, 2);
         assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
     }
 
     @Test
