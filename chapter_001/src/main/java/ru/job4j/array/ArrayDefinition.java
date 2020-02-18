267a50b5d530a28659121633ee0e0b9dc6a859bf
Into Matar <intomatar@gmail.com>
2020-02-19 02:17:02 +0600

    6.0.2. Заполнение массива.[#236697]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,13 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] shortNumbers = new short[10];
-        String[] names = new String[100500];
         float[] floatNumbers = new float[40];
+
+        String[] names = new String[3];
+        names[0] = "Roman Bukin";
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
