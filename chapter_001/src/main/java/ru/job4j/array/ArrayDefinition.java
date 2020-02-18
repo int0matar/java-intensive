0ae5593ce6b231dbee0eae8ab8f9c69c08d26e15
Into Matar <intomatar@gmail.com>
2020-02-19 01:56:15 +0600

    6.0.1. Объявление массива.[#236705]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] shortNumbers = new short[10];
+        String[] names = new String[100500];
+        float[] floatNumbers = new float[40];
+    }
+}
