a28ab0e71346c7c79b6f8efd00965d7043bb349a
Into Matar <intomatar@gmail.com>
2020-02-19 04:59:58 +0600

    6.1. Классический поиск перебором.[#236683]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -25,4 +25,14 @@ public class FindLoopTest {
         int expect = 4;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenNoFindElementOfArray() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {11, 3, 5, 4, 15, 64};
+        int value = 2;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
