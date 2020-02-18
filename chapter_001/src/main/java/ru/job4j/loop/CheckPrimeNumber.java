b4022f980b11640db6563fc4aac482b587accc17
Into Matar <intomatar@gmail.com>
2020-02-19 01:41:36 +0600

    5.5. Простое число.[#236755]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,13 +3,18 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
-        int division = 2;
+        boolean prime = true;
+
+        if (number < 2) {
+            prime = false;
+        }
 
         for(int index = 2; index < number; index++) {
-            if((number % division) != 0) {
-                division++;
+            if (number % index == 0) {
+                prime = false;
+                break;
             }
         }
-        return division == number;
+        return prime;
     }
 }
