c049a0b444d5ea2b3a4e67f17b45909fc4f82ba7
Into Matar <intomatar@gmail.com>
2020-02-19 19:22:26 +0600

    6.0.2. Заполнение массива.[#236697]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,7 +6,9 @@ public class ArrayDefinition {
         float[] floatNumbers = new float[40];
 
         String[] names = new String[3];
-        names[0] = "Roman Bukin";
+        names[0] = "James Gosling";
+        names[1] = "Guido van Rossum";
+        names[2] = "Yukihiro Matsumoto";
 
         for (int i = 0; i < 3; i++) {
             System.out.println(names[i]);
