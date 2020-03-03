f2565c22de91a0f7669a126df73a8e280d5a4c47
Into Matar <intomatar@gmail.com>
2020-03-04 04:36:25 +0600

    6.4. Слово начинается с... [#236685]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,13 +3,11 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = false;
-        char wordArr0 = word[0];
-        char wordArr1 = word[1];
-        char prefArr0 = pref[0];
-        char prefArr1 = pref[1];
 
-        if (wordArr0 == prefArr0) {
-            if (wordArr1 == prefArr1) {
+        for (int index = 0; index < pref.length; index++) {
+            if (pref[index] != word[index]) {
+                result = false;
+            } else {
                 result = true;
             }
         }
